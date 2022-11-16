d555844e47f5a936b562eb55ea9797314d4fb1d5
Olivier <olivier.winkler@icloud.com>
2022-11-16 09:30:14 +0100

    feat(content): add chakra ui login component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
+
+// import { SessionProvider } from "next-auth/react"
+
+// export default function App({
+//   Component, pageProps: { session, ...pageProps }
+// }) {
+//   return (
+//     <SessionProvider session={session}>
+//       <Component {...pageProps}/>
+//     </SessionProvider>
+//   )
+// }
